65847c418f7eb47215e47be2b2f53f3eb3c5243f
Bing <1325782809@qq.com>
2025-05-13 15:31:59 +0800

    deffered lighting

--- Render/Shaders/LightingUtil.hlsl
+++ Render/Shaders/LightingUtil.hlsl
@@ -13,20 +13,7 @@ struct LightParameters
     float SpotRadius; // Spot light only
     float2 SpotAngles; // Spot light only
     uint LightType;
-    int ShadowMapIdx;
     
-    float4x4 LightProj;
-    float4x4 ShadowTransform;
-    
-    // AreaLight
-    float3 AreaLightPoint0InWorld;
-    float LightPad2;
-    float3 AreaLightPoint1InWorld;
-    float LightPad3;
-    float3 AreaLightPoint2InWorld;
-    float LightPad4;
-    float3 AreaLightPoint3InWorld;
-    float LightPad5;
 };
 
 cbuffer LightCommonData
@@ -48,21 +35,6 @@ struct Material
     float3 FresnelR0;
     float Shininess;
 };
-
-
-float CalcDistanceAttenuation(float3 WorldPos, float3 LightPos, float Range)
-{
-    float3 WorldLightVector = LightPos - WorldPos;
-    float DistanceSqr = dot(WorldLightVector, WorldLightVector);
-    
-    float DistanceAttenuation = 1 / (DistanceSqr + 1);
-    
-    float InvRange = rcp(max(Range, 0.001f));
-    float LightRadiusMask = Square(saturate(1 - Square(DistanceSqr * InvRange * InvRange)));
-    DistanceAttenuation *= LightRadiusMask;
-    
-    return DistanceAttenuation;
-}
 
 /**
  * LightDir is the direction from shader point to spot light.
